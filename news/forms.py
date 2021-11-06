ef7e3988f59b33c49be504f8151736847dbf496f
Kanunnikov <ronddobooking@mail.ru>
2021-11-06 19:50:32 +0300

    Добавленно приложение - Корзина, а так же кнопки для Удаления и Редактирования статей

---
+++ news/forms.py
@@ -0,0 +1,26 @@
+from .models import Articles
+from django.forms import ModelForm, TextInput, DateTimeInput, Textarea
+
+class ArticlesForm(ModelForm):
+    class Meta:
+        model = Articles
+        fields = ['title', 'anonse', 'full_text', 'data']
+
+        widgets = {
+            'title': TextInput(attrs={
+                'class':'form-control',
+                'placeholder': 'Название статьи'
+            }),
+            'anonse': TextInput(attrs={
+                'class':'form-control',
+                'placeholder': 'Анонс статьи'
+            }),
+            'data': DateTimeInput(attrs={
+                'class':'form-control',
+                'placeholder': "Дата публикации"
+            }),
+            'full_text': Textarea(attrs={
+                'class':'form-control',
+                'placeholder': 'Текст статьи'
+            }),
+        }
